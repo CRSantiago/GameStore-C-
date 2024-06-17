8064c07cd6789aa577aff17acff6c3a200018774
CRSantiago <42612374+CRSantiago@users.noreply.github.com>
2024-06-17 10:57:04 -0400

    Entity Framework setup and SQLite initialization

---
+++ GameStore/Data/DataExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace GameStore.Data;
+using Microsoft.EntityFrameworkCore;
+
+public static class DataExtensions
+{
+    public static void MigrateDB(this WebApplication app) {
+        using var scope = app.Services.CreateScope();
+        var dbContex = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
+        dbContex.Database.Migrate();
+    }
+}
--- GameStore/Program.cs
+++ GameStore/Program.cs
@@ -1,8 +1,15 @@
 using GameStore.Endpoints;
+using GameStore.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connString = builder.Configuration.GetConnectionString("GameStore");
+builder.Services.AddSqlite<GameStoreContext>(connString);
+
 var app = builder.Build();
 
 app.MapGameEndpoints();
+
+app.MigrateDB();
 
 app.Run();
